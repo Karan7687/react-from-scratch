638e1980bc0777f785250c9161f5efd650f74e46
Karan <karan.l.k123321@gmail.com>
2024-07-27 20:54:03 +0530

    Add basic Idea of components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
+import GoalList from "./components/GoalList";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="course-goals">
       <h1>COURSE</h1>
-
-      <ul className="list">
-        <li>COURSE NO 1</li>
-        <li>COURSE NO 2</li>
-        <li>COURSE NO 3</li>
-        <li>COURSE NO 4</li>
-      </ul>
+      <GoalList />
     </div>
   );
 };
---
+++ src/components/GoalList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./GoalList.css"
+
+const GoalList = () => {
+  return (
+    <ul className="list">
+      <li>COURSE NO 1</li>
+      <li>COURSE NO 2</li>
+      <li>COURSE NO 3</li>
+      <li>COURSE NO 4</li>
+    </ul>
+  );
+};
+export default GoalList;
