9bceb5ec061d0618d133ded5dd40001204f11648
Karan <karan.l.k123321@gmail.com>
2024-07-28 18:40:56 +0530

    Use concept of Props in React

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import GoalList from "./components/GoalList";
 import "./App.css";
 
 const App = () => {
+  const arr = [
+    { id: "course1", text: "React Native" },
+    { id: "course2", text: "Next JS" },
+    { id: "course3", text: "GO LANG" },
+    { id: "course4", text: "RUST" },
+    //array is created to pass as props from app to GoalList
+  ];
+
   return (
     <div className="course-goals">
       <h1>COURSE</h1>
-      <GoalList />
+      <GoalList course={arr}/>
     </div>
   );
 };
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./GoalList.css"
 
-const GoalList = () => {
-  return (
+const GoalList = props => {
+
+  console.log(props.course)
+  return(
+
     <ul className="list">
-      <li>COURSE NO 1</li>
-      <li>COURSE NO 2</li>
-      <li>COURSE NO 3</li>
-      <li>COURSE NO 4</li>
+
+        {props.course.map((goal)=>{
+
+          return<li>{goal.id}</li>
+
+        })}
+
     </ul>
   );
 };
