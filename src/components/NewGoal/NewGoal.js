2ef3ee8a1c88d173eb399f47e262cebe00899194
Karan <karan.l.k123321@gmail.com>
2024-07-30 20:17:17 +0530

    Add new dummy Goal and console log

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./NewGoal.css";
 const NewGoal = () => {
   const addGoalHandler = (event) => {
-    event.preventDefault();
-    console.log("Link navigation prevented!");
+    // event.preventDefault();
+    // console.log("Link navigation prevented!");
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: "My dummy goal",
+    };
+
+    console.log(newGoal);
   };
 
   return (
