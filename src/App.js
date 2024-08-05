d099476839154ccdca25e1c0f2631b4add121f3c
Karan <karan.l.k123321@gmail.com>
2024-08-05 21:16:59 +0530

    Implement 'Usestate' to create new Goal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 import "./App.css";
 
 const App = () => {
-  const arr = [
+  const [arr, setCourseGoals] = useState([
     { id: "course1", text: "React Native" },
     { id: "course2", text: "Next JS" },
     { id: "course3", text: "GO LANG" },
     { id: "course4", text: "RUST" },
     //array is created to pass as props from app to GoalList
-  ];
+  ]);
+
+  const addGoalHandler = (NewGoal) => {
+    setCourseGoals(arr.concat(NewGoal));
+  };
+
+  // const arr = [
+  //   { id: "course1", text: "React Native" },
+  //   { id: "course2", text: "Next JS" },
+  //   { id: "course3", text: "GO LANG" },
+  //   { id: "course4", text: "RUST" },
+  //   //array is created to pass as props from app to GoalList
+  // ];
 
   return (
     <div className="course-goals">
       <h1>COURSE</h1>
-      <NewGoal />
+      <NewGoal onAddGoal={addGoalHandler} />
       <GoalList arrProp={arr} />
     </div>
   );
