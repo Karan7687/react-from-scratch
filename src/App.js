4862acf03dd3c8a6b9375191d03ebd8d40359556
Karan <karan.l.k123321@gmail.com>
2024-08-06 20:04:38 +0530

    Add More Elements to menu

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import NewGoal from "./components/NewGoal/NewGoal";
 import "./App.css";
 
 const App = () => {
-  const [arr, setCourseGoals] = useState([
+  const arr = [
     { id: "course1", text: "React Native" },
     { id: "course2", text: "Next JS" },
     { id: "course3", text: "GO LANG" },
     { id: "course4", text: "RUST" },
+    { id: "course5", text: "Kubernets" },
+    { id: "course6", text: "JAvascript" },
+    { id: "course7", text: "Java" },
+    { id: "course8", text: "Node" },
+    { id: "course9", text: "Django" },
     //array is created to pass as props from app to GoalList
-  ]);
+  ];
 
-  const addGoalHandler = (NewGoal) => {
-    setCourseGoals(arr.concat(NewGoal));
-  };
-
-  // const arr = [
-  //   { id: "course1", text: "React Native" },
-  //   { id: "course2", text: "Next JS" },
-  //   { id: "course3", text: "GO LANG" },
-  //   { id: "course4", text: "RUST" },
-  //   //array is created to pass as props from app to GoalList
-  // ];
+  const addGoalHandler = (NewGoal) => {};
 
   return (
     <div className="course-goals">
